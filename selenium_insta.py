7cc716b53d4b7a63faeb8f7592bb437b9e5a93c7
YigitYeler <74016134+YigitYeler@users.noreply.github.com>
2021-07-16 14:09:44 +0300

    Add files via upload

---
+++ selenium_insta.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import time
+myUsername = "***"
+myPassword = "***"
+
+browser = webdriver.Edge(executable_path=r'C:\Projects\msedgedriver.exe')
+
+browser.get("https://www.instagram.com")
+time.sleep(3)
+
+username = browser.find_element_by_name("username")
+password = browser.find_element_by_name("password")
+username.send_keys(myUsername)
+password.send_keys(myPassword)
+
+time.sleep(2)
+
+login = browser.find_element_by_xpath(
+    "//*[@id='loginForm']/div/div[3]/button/div")
+login.click()
+
+time.sleep(6)
+
+browser.close()
